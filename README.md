3dcb83d9a292a3c5adb2aa329ce1ca9a222bb077
melyssy <ksenyatokareva@gmail.com>
2020-11-03 22:16:22 +0300

    initial config

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 ![Actions Status](/workflows/hexlet-check/badge.svg)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/bdea7569e925f603b0bc/maintainability)](https://codeclimate.com/github/mellyssy/frontend-project-lvl3/maintainability) 
+
+# RSS агрегатор
+
+Учебный проект третьего уровня от [Hexlet](https://ru.hexlet.io/professions/frontend/projects/11).
+
+RSS-лента новостей с автоматическим обновлением.
+
+В проекте используются: JavaScript, AJAX, Webpack, Bootstrap.
---
+++ webpack.config.cjs
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '/dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
